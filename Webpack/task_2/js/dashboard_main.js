d8f3971a74bc3e4d61b8da32e7893f52a432f35a
GoldenHatchet15 <raphsant20@gmail.com>
2024-12-07 18:20:03 -0400

    add CSS & Images

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,18 +1,18 @@
-import $ from 'jquery';
-import _ from 'lodash';
-import '../css/main.css';
+import $ from "jquery";
+import _ from "lodash";
+import css from "../css/main.css";
 
-$(function() {
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button id="click-btn">Click here to get started</button>');
-    $('body').append('<p id="count"></p>');
-    $('body').append('<p>Copyright - Holberton School</p>');
+$('body').append("<p>Holberton Dashboard</p>");
+$('body').append("<p>Dashboard data for the studnts</p>");
+$('body').append("<button>Click here to get started</button>");
+$('body').append("<p id='count'></p>");
+$('body').append("<p>Copyright - Holberton School</p>");
 
-    function updateCounter() {
-        const count = parseInt($('#count').text() || '0', 10) + 1;
-        $('#count').text(`${count} clicks on the button`);
-    }
+let count = 0;
 
-    $('#click-btn').on('click', _.debounce(updateCounter, 500));
-});
+function updateCounter() {
+  count = count + 1;
+  $('#count').text(`${count.toString()} clicks on the button`);
+}
+
+$('button').click(_.debounce(updateCounter));
--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,38 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './js/dashboard_main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.html',
-            inject: true
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'assets/[name][contenthash].[ext]'
-                }
-            }
+  entry: "./js/dashboard_main",
+  mode: "production",
+  output: {
+    path:path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
         ]
-    },
-    performance: {
-        maxAssetSize: 1000000,  // Example: 1MB - adjust this based on your asset sizes
-        maxEntrypointSize: 1000000,
-        hints: 'warning'
-    }
-};
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ]
+      }
+    ]
+  }
+}
