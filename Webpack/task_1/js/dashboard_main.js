ec150bf8e091d2245e41e9933c4543151bb95538
GoldenHatchet15 <raphsant20@gmail.com>
2024-12-07 16:36:45 -0400

    Learn Webpack use

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,23 +1,17 @@
-const $ = require("jquery");
-const _ = require("lodash");
+import $ from 'jquery';
+import _ from 'lodash';
 
-let count = 0;
+$(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="click-btn">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
 
-function updateCounter() {
-  count++;
-  $("#count").text(`${count} clicks on the button`);
-}
+    function updateCounter() {
+        const count = parseInt($('#count').text() || '0', 10) + 1;
+        $('#count').text(`${count} clicks on the button`);
+    }
 
-const $button = $("<button>Click here to get started</button>").on(
-  "click",
-  _.debounce(updateCounter, 500, {
-    leading: true,
-    trailing: false,
-  })
-);
-
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append($button);
-$("body").append("<p id='count'></p>");
-$("body").append("<p>Copyright - Holberton School</p>");
+    $('#click-btn').on('click', _.debounce(updateCounter, 500));
+});
