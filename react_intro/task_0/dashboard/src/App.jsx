9368da02a35a3a60a32dc7117171312c996e2ddb
GoldenHatchet15 <raphsant20@gmail.com>
2024-12-20 12:30:23 -0400

    Embedding expressions, funct

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css'; // Make sure the path is correct
 import logo from './assets/holberton-logo.jpg'; // Adjust the path as needed
+import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
 
 function App() {
   return (
     <div className="App">
+      <Notifications />
       <header className="App-header">
         <img src={logo} alt="Holberton logo" />
         <h1>School dashboard</h1>
@@ -13,7 +17,7 @@ function App() {
         <p>Login to access the full dashboard</p>
       </div>
       <footer className="App-footer">
-        <p>Copyright 2024 - Holberton School</p>
+        <p>{getFooterCopy(true)} - Copyright {getCurrentYear()} - Holberton School</p>
       </footer>
     </div>
   );
