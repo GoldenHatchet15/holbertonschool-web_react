7057d3ff94b45aaf043e390851a35c2000a2d583
GoldenHatchet15 <raphsant20@gmail.com>
2024-12-22 20:50:48 -0400

    Reorganize

--- react_intro/task_5/dashboard/eslint.config.js
+++ react_intro/task_5/dashboard/eslint.config.js
@@ -1,42 +1,44 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals';
+import babelParser from '@babel/eslint-parser';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefreshPlugin from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    ignores: ['dist/**'], // Ignore the dist folder
+    files: ['**/*.js', '**/*.jsx'], // Apply this configuration to JS and JSX files
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: babelParser, // Use Babel parser
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      }
-    },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+        ecmaVersion: 2020, // ECMAScript 2020 syntax
+        sourceType: 'module', // Use ECMAScript modules
+        requireConfigFile: false, // Prevents errors if Babel config is not explicitly defined
+        babelOptions: {
+          presets: ['@babel/preset-react'], // Add React preset
+        },
       },
       globals: {
-        ...globals.browser,
-        jest: true,  // This tells ESLint that Jest globals should be treated as defined
-      }
+        ...globals.browser, // Add browser global variables
+        jest: 'readonly', // Add Jest global variables for tests
+      },
     },
-]
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect React version
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-refresh': reactRefreshPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react/react-in-jsx-scope': 'off', // Disable the outdated rule
+    },
+  },
+];
