37a6c11079078e171270d93931c01d8bdac0c7d4
GoldenHatchet15 <raphsant20@gmail.com>
2024-12-22 23:39:30 -0400

    task 1

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, act } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from './Login';
 
 test('renders 2 labels, 2 inputs, and 1 button', () => {
@@ -7,18 +8,17 @@ test('renders 2 labels, 2 inputs, and 1 button', () => {
   const inputs = getAllByTestId('input-element');
   const button = getByText('OK');
 
-  expect(inputs).toHaveLength(2); // Check for 2 input elements
+  expect(inputs).toHaveLength(2); // Check for 2 inputs
   expect(button).toBeInTheDocument(); // Check for button
 });
 
-test('input gets focused when label is clicked', () => {
+test('input gets focused when label is clicked', async () => {
+  const user = userEvent.setup(); // Create a user event simulation
   const { getByLabelText } = render(<Login />);
   const emailLabel = getByLabelText('Email:');
   const emailInput = getByLabelText('Email:');
 
-  act(() => {
-    emailLabel.click();
-  });
+  await user.click(emailLabel); // Simulate user clicking on the label
 
-  expect(emailInput).toHaveFocus();
+  expect(emailInput).toHaveFocus(); // Check if the input is focused
 });
