16a81b6ee382ee8f87c14c42608dc9e35533b5fd
GoldenHatchet15 <raphsant20@gmail.com>
2024-12-22 23:31:10 -0400

    task 1

---
+++ react_props/task_1/dashboard/src/App/App.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+
+const App = () => (
+  <Fragment>
+    <Header />
+    <Login />
+    <Footer />
+  </Fragment>
+);
+
+export default App;
